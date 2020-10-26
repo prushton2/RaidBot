5d9cdbb2503123121309e9c3c7d6bc0505bab82c
prushton2 <peterrushton52@gmail.com>
2020-10-26 11:25:58 -0400

    Added role modifications

--- main.py
+++ main.py
@@ -7,27 +7,66 @@ import os
 import colorama
 import time
 
-from cogs import colors, extraRoles
 
-# jsm = __import__("JsonManager")
-# pyc = __import__("pyconfig")
-# Items = __import__("Items")
-# Video = __import__("Video")
+def getRoleByName(ctx, name):
+    allServerRoles = ctx.guild.roles
+    for i in allServerRoles:
+        if(i.name.lower() == name.lower()):
+            return i
 
-# config = jsm.JsonManager(pyc.configPath)
-# userdata = jsm.UserData(pyc.userDataPath)
+pyc = __import__("pyconfig")
+jsm = __import__("jsonmanager")
+
+config = jsm.JsonManager(pyc.configPath)
+commandJsonClass = jsm.JsonManager(pyc.commandsPath)
+
+commandNames = []
+
+for key in commandJsonClass.load():
+    commandNames.append(key)
 
 bot = commands.Bot(command_prefix= config.load()["prefix"])
-
-cogs = [Default.Default, Economy.Economy, Inventory.Inventory, Voice.Voice, onMessage.onMessage]
 
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name=config.load()["prefix"]+"help"))
     print("Bot is ready")
 
+@bot.event
+async def on_message(ctx):
+    if(ctx.author == bot.user): 
+        return
 
-for i in cogs:
-    bot.add_cog(i(bot))
+    prefix = config.load()["prefix"]
+    syntaxArray = ctx.content.split(" ")
+    syntax = " ".join(syntaxArray[1:])
+    commandJson = commandJsonClass.load()
 
+    if(not ctx.content.startswith(prefix)):
+        return
+
+
+    for command in commandNames:
+        if(ctx.content.startswith(prefix+command)):
+            command = commandJson[command]
+            userRoles = ctx.author.roles
+
+            if( not (command["requiredRole"] in [i.name.lower() for i in userRoles])):
+                await ctx.channel.send("You dont have permission to use this command")
+                return
+
+            roleNeedsRemoval = syntax in [i.name.lower() for i in userRoles]
+            
+            if(command["allowedOneRole"]):
+                for role in command["roles"]:
+                    if(role in [i.name.lower() for i in userRoles]):
+                        await ctx.author.remove_roles(getRoleByName(ctx, role))
+
+            if(not roleNeedsRemoval):
+                await ctx.author.add_roles(getRoleByName(ctx, syntax))
+            else:
+                await ctx.author.remove_roles(getRoleByName(ctx, syntax))
+
+
+            return
 bot.run(config.load()["token"])
