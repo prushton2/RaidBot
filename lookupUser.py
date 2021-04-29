c55e957e613bc86efeddc2373f15bda37a078cd5
prushton2 <peterrushton52@gmail.com>
2021-04-29 00:25:13 -0400

    Put the code into functions the functions get your collections and check if you have certain weapons

--- lookupUser.py
+++ lookupUser.py
@@ -3,47 +3,57 @@ import requests
 import json
 import os
 
-jsonmanager = __import__("JsonManager")
+
 
 HEADERS = {"X-API-Key" : "d16ea396820e48e7bfec254ab512eec4"}
 
-workingFilejson = jsonmanager.JsonManager("C:\\Users\\Peter\\Documents\\code\\py\\Bungie net api\\workingFile.json")
-outputjson = jsonmanager.JsonManager("C:\\Users\\Peter\\Documents\\code\\py\\Bungie net api\\output.json")
 
-membershipType = -1
 root_url = "https://www.bungie.net/Platform"
-displayName = "Soul2956"
 
+#Items I care about
 importantItems = {"Anarchy":"2220014607", "Divinity":"1988948484", "Eyes of Tomorrow":"753200559", "Legend of Acrius":"199171389", "One Thousand Voices":"199171385", "Tarrabah":"2329697053"}
+#Enum of what the "state" key means
 collectibleState = {0:"Acquired", 1:"Not Acquired", 2:"Obscured", 4:"Invisible", 8:"Cannot Afford Material Requirements", 16:"Inventory Space Unavailable", 32:"Uniqueness Violation", 64:"Purchase Disabled"}
 
-## Get the players membershipId
-url = f"/Destiny2/SearchDestinyPlayer/{membershipType}/{displayName}" 
-res = requests.get(root_url+url, headers=HEADERS)
-userInfo = json.loads(res.text)["Response"][0]
 
-#Get more data from the membershipId
-url = f"/Destiny2/{userInfo['membershipType']}/Profile/{userInfo['membershipId']}/?components=100" 
-res = requests.get(root_url+url, headers=HEADERS)
-
-##save data in userInfo
-userInfo = json.loads(res.text)["Response"]
-
-characterId = userInfo['profile']['data']['characterIds'][0]
-membershipType = userInfo['profile']['data']['userInfo']['membershipType']
-destinyMembershipId = userInfo['profile']['data']['userInfo']['membershipId']
-
-# print(membershipType, des)
-
-url = f"/Destiny2/{membershipType}/Profile/{destinyMembershipId}/?components=800"
-res = requests.get(root_url+url, headers=HEADERS)
-
-outputjson.save(json.loads(res.text))
-
-collections = json.loads(res.text)["Response"]["profileCollectibles"]["data"]["collectibles"]
+#This is the location of the collections menu incase I forget
+# collections = json.loads(res.text)["Response"]["profileCollectibles"]["data"]["collectibles"]
 
 
-for i in importantItems:
-    j = importantItems[i]
-    print(f"{i}: ", end="")
-    print(collectibleState[collections[j]["state"]])
+
+def getCollections(username):
+    ## Get the players membershipId
+    url = f"/Destiny2/SearchDestinyPlayer/-1/{username}" 
+    res = requests.get(root_url+url, headers=HEADERS)
+    userInfo = json.loads(res.text)["Response"][0]
+
+
+    #Get more data from the membershipId and Type
+    url = f"/Destiny2/{userInfo['membershipType']}/Profile/{userInfo['membershipId']}/?components=100" 
+    res = requests.get(root_url+url, headers=HEADERS)
+
+    userInfo = json.loads(res.text)["Response"]
+
+    #Condense the JSON into variables for ease of use
+    membershipType = userInfo['profile']['data']['userInfo']['membershipType']
+    destinyMembershipId = userInfo['profile']['data']['userInfo']['membershipId']
+
+    #Get the users collections info
+    url = f"/Destiny2/{membershipType}/Profile/{destinyMembershipId}/?components=800"
+    res = requests.get(root_url+url, headers=HEADERS)
+
+    return json.loads(res.text)["Response"]
+
+def getImportantItemStatus(username):
+    
+    collections = getCollections(username)["profileCollectibles"]["data"]["collectibles"]
+
+    collectionsState = {} #Holds the status of the collections state of the items I care about. This is returned to the user 
+
+    for i in importantItems:
+        j = importantItems[i]
+        collectionsState[i] = (collections[j]["state"] == 0)
+
+    return collectionsState
+
+print(getImportantItemStatus("Master Oobway"))
